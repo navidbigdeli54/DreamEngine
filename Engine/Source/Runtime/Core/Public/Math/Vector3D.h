db39323800450ea0d3b842dd29abd30874860445
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-04-05 16:49:44 +0430

    added vector3d tests

--- Engine/Source/Runtime/Core/Public/Math/Vector3D.h
+++ Engine/Source/Runtime/Core/Public/Math/Vector3D.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Math/Vector2D.h"
 #include "Platform/Platform.h"
 #include "Platform/PlatformMath.h"
 
@@ -36,10 +35,6 @@ public:
 
 	FORCEINLINE Vector3D(const Vector3D &vector);
 
-	FORCEINLINE explicit Vector3D(const Vector2D &vector);
-
-	FORCEINLINE explicit Vector3D(const Vector2D &vector, float z);
-
 public:
 
 	FORCEINLINE Vector3D operator^(const Vector3D &rhs) const;
@@ -62,9 +57,9 @@ public:
 
 	FORCEINLINE void Set(float x, float y, float z);
 
-	FORCEINLINE float Size() const;
+	FORCEINLINE float Magnitude() const;
 
-	FORCEINLINE float SizeSquared() const;
+	FORCEINLINE float MagnitudeSquared() const;
 
 	FORCEINLINE bool IsZero() const;
 
@@ -107,10 +102,6 @@ FORCEINLINE Vector3D::Vector3D(float x, float y, float z) : X(x), Y(y), Z(z) { }
 
 FORCEINLINE Vector3D::Vector3D(const Vector3D &vector) : X(vector.X), Y(vector.Y), Z(vector.Z) { }
 
-FORCEINLINE Vector3D::Vector3D(const Vector2D &vector) : X(vector.X), Y(vector.Y), Z(0) { }
-
-FORCEINLINE Vector3D::Vector3D(const Vector2D &vector, float inZ) : X(vector.X), Y(vector.Y), Z(inZ) { }
-
 FORCEINLINE Vector3D Vector3D::operator^(const Vector3D &rhs) const
 {
 	return Vector3D(
@@ -127,7 +118,7 @@ FORCEINLINE float Vector3D::operator*(const Vector3D &rhs) const
 
 FORCEINLINE Vector3D Vector3D::operator*(const float scale) const
 {
-	return Vector3D(X * scale, Y * scale, X * scale);
+	return Vector3D(X * scale, Y * scale, Z * scale);
 }
 
 FORCEINLINE Vector3D Vector3D::operator*=(const float scale)
@@ -142,7 +133,7 @@ FORCEINLINE Vector3D Vector3D::operator/(const float scale) const
 {
 	const float reversedScale = 1.f / scale;
 
-	return Vector3D(X * reversedScale, Y * reversedScale, X * reversedScale);
+	return Vector3D(X * reversedScale, Y * reversedScale, Z * reversedScale);
 }
 
 FORCEINLINE Vector3D Vector3D::operator/=(const float scale)
@@ -173,12 +164,12 @@ FORCEINLINE void Vector3D::Set(float inX, float inY, float inZ)
 	Z = inZ;
 }
 
-FORCEINLINE float Vector3D::Size() const
+FORCEINLINE float Vector3D::Magnitude() const
 {
-	return Math::Sqrt(SizeSquared());
+	return Math::Sqrt(MagnitudeSquared());
 }
 
-FORCEINLINE float Vector3D::SizeSquared() const
+FORCEINLINE float Vector3D::MagnitudeSquared() const
 {
 	return X * X + Y * Y + Z * Z;
 }
@@ -195,7 +186,7 @@ FORCEINLINE bool Vector3D::IsNearlyZero(float tolerance) const
 
 FORCEINLINE bool Vector3D::Normalize(float tolerance)
 {
-	float sqrtSize = SizeSquared();
+	float sqrtSize = MagnitudeSquared();
 
 	if (sqrtSize > tolerance)
 	{
@@ -211,19 +202,19 @@ FORCEINLINE bool Vector3D::Normalize(float tolerance)
 
 FORCEINLINE bool Vector3D::IsNormalize() const
 {
-	return Math::Abs(1.f - SizeSquared()) < FLOAT_PRECESION_THRESHOLD;
+	return Math::Abs(1.f - MagnitudeSquared()) < FLOAT_PRECESION_THRESHOLD;
 }
 
 FORCEINLINE Vector3D Vector3D::GetUnsafeNormal() const
 {
-	const float reversedScale = 1.f / Size();
+	const float reversedScale = 1.f / Magnitude();
 
 	return *this / reversedScale;
 }
 
 FORCEINLINE Vector3D Vector3D::GetSafeNormal(float tolerance) const
 {
-	const float sqrtSize = SizeSquared();
+	const float sqrtSize = MagnitudeSquared();
 
 	if (sqrtSize == 1) return *this;
 	else if (sqrtSize < tolerance) return Zero;
@@ -252,5 +243,3 @@ FORCEINLINE float Vector3D::DistanceSquared(const Vector3D &a, const Vector3D &b
 {
 	return Math::Square(b.X - a.X) + Math::Square(b.Y - a.Y) + Math::Sqrt(b.Z - a.Z);
 }
-
-FORCEINLINE Vector2D::Vector2D(const Vector3D &inVector) : X(inVector.X), Y(inVector.Y) { }
---
+++ Engine/Source/Test/Runtime/Core/Math/Vector3DTest.cpp
@@ -0,0 +1,282 @@
+#include <catch2/catch_test_macros.hpp>
+#include "Math/Vector3D.h"
+
+TEST_CASE("Vector3D::Zero", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::Zero;
+    REQUIRE(vec3.X == 0);
+    REQUIRE(vec3.Y == 0);
+    REQUIRE(vec3.Z == 0);
+}
+
+TEST_CASE("Vector3D::One", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::One;
+    REQUIRE(vec3.X == 1);
+    REQUIRE(vec3.Y == 1);
+    REQUIRE(vec3.Z == 1);
+}
+
+TEST_CASE("Vector3D::Up", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::Up;
+    REQUIRE(vec3.X == 0);
+    REQUIRE(vec3.Y == 1);
+    REQUIRE(vec3.Z == 0);
+}
+
+TEST_CASE("Vector3D::Forward", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::Forward;
+    REQUIRE(vec3.X == 0);
+    REQUIRE(vec3.Y == 0);
+    REQUIRE(vec3.Z == 1);
+}
+
+TEST_CASE("Vector3D::Right", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::Right;
+    REQUIRE(vec3.X == 1);
+    REQUIRE(vec3.Y == 0);
+    REQUIRE(vec3.Z == 0);
+}
+
+TEST_CASE("Vector3D::Vector3D(float)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float random = rand() % 100;
+    Vector3D vec3(random);
+    REQUIRE(Math::AlmostEqual(vec3.X, random));
+    REQUIRE(Math::AlmostEqual(vec3.Y, random));
+    REQUIRE(Math::AlmostEqual(vec3.Z, random));
+}
+
+TEST_CASE("Vector3D::Vector3D(float, float, float)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float randX = rand() % 100;
+    float randY = rand() % 100;
+    float randZ = rand() % 100;
+
+    Vector3D vec3(randX, randY, randZ);
+    REQUIRE(Math::AlmostEqual(vec3.X, randX));
+    REQUIRE(Math::AlmostEqual(vec3.Y, randY));
+    REQUIRE(Math::AlmostEqual(vec3.Z, randZ));
+}
+
+TEST_CASE("Vector3D::Vector3D(&Vector3D)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float randX = rand() % 100;
+    float randY = rand() % 100;
+    float randZ = rand() % 100;
+
+    Vector3D vec3(randX, randY, randZ);
+
+    Vector3D otherVec3(vec3);
+
+    REQUIRE(Math::AlmostEqual(otherVec3.X, randX));
+    REQUIRE(Math::AlmostEqual(otherVec3.Y, randY));
+    REQUIRE(Math::AlmostEqual(otherVec3.Z, randZ));
+}
+
+TEST_CASE("Vector3D::CrossProduct(&Vector3D)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float vec1RandX = rand() % 100;
+    float vec1RandY = rand() % 100;
+    float vec1RandZ = rand() % 100;
+    Vector3D vec1(vec1RandX, vec1RandY, vec1RandZ);
+
+    float vec2RandX = rand() % 100;
+    float vec2RandY = rand() % 100;
+    float vec2RandZ = rand() % 100;
+
+    Vector3D vec2(vec2RandX, vec2RandY, vec2RandZ);
+
+    Vector3D crossProduct = vec1 ^ vec2;
+
+    REQUIRE(Math::AlmostEqual(crossProduct.X, vec1.Y * vec2.Z - vec1.Z * vec2.Y));
+    REQUIRE(Math::AlmostEqual(crossProduct.Y, vec1.Z * vec2.X - vec1.X * vec2.Z));
+    REQUIRE(Math::AlmostEqual(crossProduct.Z, vec1.X * vec2.Y - vec1.Y * vec2.X));
+}
+
+TEST_CASE("Vector3D::DotProduct(&Vector3D)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float vec1RandX = rand() % 100;
+    float vec1RandY = rand() % 100;
+    float vec1RandZ = rand() % 100;
+    Vector3D vec1(vec1RandX, vec1RandY, vec1RandZ);
+
+    float vec2RandX = rand() % 100;
+    float vec2RandY = rand() % 100;
+    float vec2RandZ = rand() % 100;
+
+    Vector3D vec2(vec2RandX, vec2RandY, vec2RandZ);
+
+    float dotProduct = vec1 * vec2;
+
+    REQUIRE(Math::AlmostEqual(dotProduct, vec1.X * vec2.X + vec1.Y * vec2.Y + vec1.Z * vec2.Z));
+}
+
+TEST_CASE("Vector3D::Scale(float)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float vec1RandX = rand() % 100;
+    float vec1RandY = rand() % 100;
+    float vec1RandZ = rand() % 100;
+    Vector3D vec3(vec1RandX, vec1RandY, vec1RandZ);
+
+    float randScale = rand() % 100;
+
+    Vector3D scaledVec3 = vec3 * randScale;
+
+    REQUIRE(Math::AlmostEqual(scaledVec3.X, vec3.X * randScale));
+    REQUIRE(Math::AlmostEqual(scaledVec3.Y, vec3.Y * randScale));
+    REQUIRE(Math::AlmostEqual(scaledVec3.Z, vec3.Z * randScale));
+}
+
+TEST_CASE("Vector3D::ScaleAndAssign(float)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float vec1RandX = rand() % 100;
+    float vec1RandY = rand() % 100;
+    float vec1RandZ = rand() % 100;
+    Vector3D vec3(vec1RandX, vec1RandY, vec1RandZ);
+
+    float randScale = rand() % 100;
+
+    Vector3D temp(vec3);
+    temp *= randScale;
+
+    REQUIRE(Math::AlmostEqual(temp.X, vec3.X * randScale));
+    REQUIRE(Math::AlmostEqual(temp.Y, vec3.Y * randScale));
+    REQUIRE(Math::AlmostEqual(temp.Z, vec3.Z * randScale));
+}
+
+TEST_CASE("Vector3D::Divide(float)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float vec1RandX = rand() % 100;
+    float vec1RandY = rand() % 100;
+    float vec1RandZ = rand() % 100;
+    Vector3D vec3(vec1RandX, vec1RandY, vec1RandZ);
+
+    float randScale = rand() % 100;
+
+    Vector3D scaledVec3 = vec3 / randScale;
+
+    REQUIRE(Math::AlmostEqual(scaledVec3.X, vec3.X / randScale));
+    REQUIRE(Math::AlmostEqual(scaledVec3.Y, vec3.Y / randScale));
+    REQUIRE(Math::AlmostEqual(scaledVec3.Z, vec3.Z / randScale));
+}
+
+TEST_CASE("Vector3D::DivideAndAssign(float)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float vec1RandX = rand() % 100;
+    float vec1RandY = rand() % 100;
+    float vec1RandZ = rand() % 100;
+    Vector3D vec3(vec1RandX, vec1RandY, vec1RandZ);
+
+    float randScale = rand() % 100;
+
+    Vector3D temp(vec3);
+    temp /= randScale;
+
+    REQUIRE(Math::AlmostEqual(temp.X, vec3.X / randScale));
+    REQUIRE(Math::AlmostEqual(temp.Y, vec3.Y / randScale));
+    REQUIRE(Math::AlmostEqual(temp.Z, vec3.Z / randScale));
+}
+
+TEST_CASE("Vector3D::Equal(&Vector3D)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float vec1RandX = rand() % 100;
+    float vec1RandY = rand() % 100;
+    float vec1RandZ = rand() % 100;
+    Vector3D vec1(vec1RandX, vec1RandY, vec1RandZ);
+
+    Vector3D vec2(vec1RandX, vec1RandY, vec1RandZ);
+
+    REQUIRE(vec1 == vec2);
+}
+
+TEST_CASE("Vector3D::NotEqual(&Vector3D)", "[Vector3D]")
+{
+    srand(time(NULL));
+    float vec1RandX = rand() % 100;
+    float vec1RandY = rand() % 100;
+    float vec1RandZ = rand() % 100;
+    Vector3D vec1(vec1RandX, vec1RandY, vec1RandZ);
+
+    Vector3D vec2(vec1RandX + rand() % 100, vec1RandY + rand() % 100, vec1RandZ + rand() % 100);
+
+    REQUIRE(vec1 != vec2);
+}
+
+TEST_CASE("Vector3D::Set", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::Zero;
+
+    srand(time(NULL));
+    float randX = rand() % 100;
+    float randY = rand() % 100;
+    float randZ = rand() % 100;
+
+    vec3.Set(randX, randY, randZ);
+    REQUIRE(Math::AlmostEqual(vec3.X, randX));
+    REQUIRE(Math::AlmostEqual(vec3.Y, randY));
+    REQUIRE(Math::AlmostEqual(vec3.Z, randZ));
+}
+
+TEST_CASE("Vector3D::Magnitude", "[Vector3D]")
+{
+    srand(time(NULL));
+    float randX = rand() % 100;
+    float randY = rand() % 100;
+    float randZ = rand() % 100;
+    Vector3D vec3(randX, randY, randZ);
+    REQUIRE(Math::AlmostEqual(vec3.Magnitude(), Math::Sqrt(randX * randX + randY * randY + randZ * randZ)));
+}
+
+TEST_CASE("Vector3D::MagnitudeSquared", "[Vector3D]")
+{
+    srand(time(NULL));
+    float randX = rand() % 100;
+    float randY = rand() % 100;
+    float randZ = rand() % 100;
+    Vector3D vec3(randX, randY, randZ);
+    REQUIRE(Math::AlmostEqual(vec3.MagnitudeSquared(), randX * randX + randY * randY + randZ * randZ));
+}
+
+TEST_CASE("Vector3D::IsZero", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::Zero;
+    REQUIRE(vec3.IsZero());
+}
+
+TEST_CASE("Vector3D::IsNotZero", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::One;
+    REQUIRE_FALSE(vec3.IsZero());
+}
+
+TEST_CASE("Vector3D::IsNearlyZero", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::Zero;
+    REQUIRE(vec3.IsNearlyZero());
+}
+
+TEST_CASE("Vector3D::IsNearZeroNumberNearlyZero", "[Vector3D]")
+{
+    Vector3D vec3(NEAR_ZERO_NUMBER, NEAR_ZERO_NUMBER, NEAR_ZERO_NUMBER);
+    REQUIRE(vec3.IsNearlyZero());
+}
+
+TEST_CASE("Vector3D::IsNotNearlyZero", "[Vector3D]")
+{
+    Vector3D vec3 = Vector3D::One;
+    REQUIRE_FALSE(vec3.IsNearlyZero());
+}
