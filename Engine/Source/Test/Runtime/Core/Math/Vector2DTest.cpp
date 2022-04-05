66e6fe1835838bb52a1426c183f3a3a23a5c6a87
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-04-05 13:39:47 +0430

    Added some tests for Vector2D

---
+++ Engine/Source/Test/Runtime/Core/Math/Vector2DTest.cpp
@@ -0,0 +1,58 @@
+#include <catch2/catch_test_macros.hpp>
+#include "Math/Vector2D.h"
+
+TEST_CASE("Set", "[Vector2D]")
+{
+    Vector2D vec2 = Vector2D::Zero;
+    vec2.Set(5.0f, 3.0f);
+    REQUIRE(vec2.X == 5.0f);
+    REQUIRE(vec2.Y == 3.0f);
+}
+
+TEST_CASE("Size", "[Vector2D]")
+{
+    srand(time(NULL));
+    float randX = rand() % 100;
+    float randY = rand() % 100;
+    Vector2D vec2(randX, randY);
+    REQUIRE(vec2.Size() == Math::Sqrt(randX * randX + randY * randY));
+}
+
+TEST_CASE("SizeSqure", "[Vector2]")
+{
+    srand(time(NULL));
+    float randX = rand() % 100;
+    float randY = rand() % 100;
+    Vector2D vec2(randX, randY);
+    REQUIRE(vec2.SizeSquared() == randX * randX + randY * randY);
+}
+
+TEST_CASE("IsZero", "[Vector2D]")
+{
+    Vector2D vec2 = Vector2D::Zero;
+    REQUIRE(vec2.IsZero());
+}
+
+TEST_CASE("IsNotZero", "[Vector2D]")
+{
+    Vector2D vec2 = Vector2D::One;
+    REQUIRE_FALSE(vec2.IsZero());
+}
+
+TEST_CASE("IsNearlyZero", "[Vector2D]")
+{
+    Vector2D vec2 = Vector2D::Zero;
+    REQUIRE(vec2.IsNearlyZero());
+}
+
+TEST_CASE("IsNearZeroNumberNearlyZero", "[Vector2D]")
+{
+    Vector2D vec2(NEAR_ZERO_NUMBER, NEAR_ZERO_NUMBER);
+    REQUIRE(vec2.IsNearlyZero());
+}
+
+TEST_CASE("IsNotNearlyZero", "[Vector2D]")
+{
+    Vector2D vec2 = Vector2D::One;
+    REQUIRE_FALSE(vec2.IsNearlyZero());
+}
