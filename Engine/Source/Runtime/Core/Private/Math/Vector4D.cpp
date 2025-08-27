bb15aa1b3a12c09eb44b4b2edd12c39351afe6c2
Navid Bigdeli <navidbigdeli54@gmail.com>
2025-08-27 09:22:57 +0330

    Implemented minimum functionlaities for Vector4D class.

---
+++ Engine/Source/Runtime/Core/Private/Math/Vector4D.cpp
@@ -0,0 +1,74 @@
+#include "Math/Vector4D.h"
+
+//-------------------------------------------------------------------------------------------------
+
+FVector4D::FVector4D(float InX, float InY, float InZ, float InW) : X(InX), Y(InY), Z(InZ), W(InW)
+{
+
+}
+
+//-------------------------------------------------------------------------------------------------
+
+float& FVector4D::operator[](int Component)
+{
+	return ((&X)[Component]);
+}
+
+//-------------------------------------------------------------------------------------------------
+
+const float& FVector4D::operator[](int Component) const
+{
+	return ((&X)[Component]);
+}
+
+//-------------------------------------------------------------------------------------------------
+
+FVector4D& FVector4D::operator*=(float Scalar)
+{
+	X *= Scalar;
+	Y *= Scalar;
+	Z *= Scalar;
+	W *= Scalar;
+
+	return *this;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+FVector4D& FVector4D::operator/=(float Scalar)
+{
+	const float Factor = 1.f / Scalar;
+
+	X *= Factor;
+	Y *= Factor;
+	Z *= Factor;
+	W *= Factor;
+
+	return *this;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+FVector4D& FVector4D::operator+=(const FVector4D& Other)
+{
+	X += Other.X;
+	Y += Other.Y;
+	Z += Other.Z;
+	W += Other.W;
+
+	return *this;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+FVector4D& FVector4D::operator-=(const FVector4D& Other)
+{
+	X -= Other.X;
+	Y -= Other.Y;
+	Z -= Other.Z;
+	W -= Other.W;
+
+	return *this;
+}
+
+//-------------------------------------------------------------------------------------------------
--- Engine/Source/Runtime/Core/Public/Math/Vector4D.h
+++ Engine/Source/Runtime/Core/Public/Math/Vector4D.h
@@ -2,7 +2,28 @@
 
 struct FVector4D
 {
+
 public:
 
 	float X, Y, Z, W;
+
+public:
+
+	FVector4D() = default;
+
+	FVector4D(float InX, float InY, float InZ, float InW);
+
+public:
+
+	float& operator[](int Component);
+
+	const float& operator[](int Component) const;
+
+	FVector4D& operator*=(float Scalar);
+
+	FVector4D& operator/=(float Scalar);
+
+	FVector4D& operator +=(const FVector4D& Other);
+
+	FVector4D& operator -=(const FVector4D& Other);
 };
